76a06306c4ae89f8697c81fb7fbe0db7719195f9
Cleclemi28 <c.trofleau@gmail.com>
2017-03-23 14:40:24 +0100

    inital commit

---
+++ generated/angular/template-cache.js
@@ -0,0 +1,62 @@
+angular.module("app").run(["$templateCache", function($templateCache) {
+
+  $templateCache.put("anon/home.html",
+    ""
+  );
+
+  $templateCache.put("anon/navbar.html",
+    "<nav class=\"navbar navbar-default\" role=\"navigation\" ng-controller=\"NavbarController\">\n" +
+    "    <div class=\"container\">\n" +
+    "        <div class=\"navbar-header\">\n" +
+    "            <a class=\"navbar-brand\" href=\"#\">Invitation au voyage</a>\n" +
+    "        </div>\n" +
+    "        <div id=\"navbar\">\n" +
+    "            <form class=\"navbar-form navbar-left\">\n" +
+    "                <div class=\"form-group\">\n" +
+    "                    <input type=\"text\" class=\"form-control\" placeholder=\"Search\">\n" +
+    "                </div>\n" +
+    "                <button type=\"submit\" class=\"btn btn-default\">Submit</button>\n" +
+    "            </form>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</nav>\n"
+  );
+
+  $templateCache.put("user/dashboard.html",
+    "Dashboard de {{user.email}}\n"
+  );
+
+  $templateCache.put("user/navbar.html",
+    "<nav class=\"navbar navbar-default\" role=\"navigation\" ng-controller=\"NavbarController\">\n" +
+    "    <div class=\"container-fluid\">\n" +
+    "        <div class=\"navbar-header\">\n" +
+    "            <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#navbar\">\n" +
+    "        <span class=\"sr-only\">Toggle navigation</span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "        <span class=\"icon-bar\"></span>\n" +
+    "      </button>\n" +
+    "            <a class=\"navbar-brand\" href=\"#\"></a>\n" +
+    "        </div>\n" +
+    "        <div class=\"collapse navbar-collapse\" id=\"navbar\">\n" +
+    "            <ul class=\"nav navbar-nav\">\n" +
+    "                <li ui-sref-active=\"active\"><a ui-sref=\"user.dashboard\" ng-show=\"auth.isAuthenticated()\">Dashboard</a></li>\n" +
+    "                <li ui-sref-active=\"active\"><a ui-sref=\"user.profile\" ng-show=\"auth.isAuthenticated()\">Profile</a></li>\n" +
+    "\n" +
+    "            </ul>\n" +
+    "            <ul class=\"nav navbar-nav navbar-right\">\n" +
+    "                <li>\n" +
+    "                <li ui-sref-active=\"active\"><a ui-sref=\"anon.home\">Website</a></li>\n" +
+    "                    <li><a ng-click=\"logout()\" ng-show=\"auth.isAuthenticated()\" href='#'>Logout</a></li>\n" +
+    "                </li>\n" +
+    "            </ul>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</nav>\n"
+  );
+
+  $templateCache.put("user/profile.html",
+    "Profile de {{user.email}}\n"
+  );
+
+}]);
